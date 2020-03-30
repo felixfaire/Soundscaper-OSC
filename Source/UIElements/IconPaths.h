712355147eb24af59f326e1900f1aa0d7a0e7721
Felix Faire <felixfaire@gmail.com>
2020-03-30 15:03:23 +0100

    added icon paths

---
+++ Source/UIElements/IconPaths.h
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    IconPaths.h
+    Created: 27 Mar 2020 3:35:46pm
+    Author:  Felix Faire
+
+  ==============================================================================
+*/
+
+#pragma once
+    
+#include <JuceHeader.h>
+
+class IconPaths
+{
+public:
+    static Path getTopPath();
+    static Path getSideOnPath();
+    static Path getBackPath();
+};
